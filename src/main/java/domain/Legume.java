66a99238fca2dee3e556503f161737a1bdb89259
Daniel Sanches <dh.sanchesp@gmail.com>
2022-08-27 02:50:22 -0300

    Creating Legumes resource

---
+++ src/main/java/domain/Legume.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Legume {
+    public String name;
+    public String description;
+
+    public Legume() {
+    }
+
+    public Legume(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+}
